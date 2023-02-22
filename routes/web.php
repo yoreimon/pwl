2847ff3fe400e2bd4451b5ac83fa94ce42ee1039
JosafatPratamaSusilo <susilojosafat@gmail.com>
2023-02-22 14:27:32 +0700

    Praktikum 1

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    echo "Selamat Datang";
+});
+
+Route::get('/about', function () {
+    echo "2141720031 / Josafat Pratama Susilo";
+});
+
+Route::get('/articles/{id}', function ($id) {
+    echo "Halaman Artikel dengan ID $id";
 });
